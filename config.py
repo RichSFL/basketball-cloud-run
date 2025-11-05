4a62b87d60dd7213464866d582d8a78c98bdf4ac
Ricahrd Inga <richardingach@gmail.com>
2025-11-05 21:37:45 +0000

    Simplify config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
-# Paste the config.py code here
+"""Configuration"""
+import os
+
+# API Config
+API_TOKEN = os.getenv('API_TOKEN', 'test-token')
+LEAGUE_ID = os.getenv('LEAGUE_ID', '1')
+SPORT_ID = os.getenv('SPORT_ID', '2')
+
+# GCS Config
+GCS_PROJECT_ID = os.getenv('GCS_PROJECT', 'basketball-projections-python')
+GCS_BUCKET = os.getenv('GCS_BUCKET', 'basketball-projections')
+
+# Game Config
+QUARTER_SECONDS = 720
+OT_SECONDS = 300
+TIMEZONE = 'America/New_York'
+
+# Thresholds
+LEADER_THRESHOLD_POINTS = 5
+LEADER_THRESHOLD_REBOUNDS = 3
+LEADER_THRESHOLD_ASSISTS = 3
